415a0942a8c8b882cd74a2d23443174e249028da
Hyun A Kim <hyuna@Violet.local>
2023-12-28 20:02:30 +0900

    setup: Tabs

---
+++ madcampProj1/src/screens/GalleryScreen/index.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function ContactScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>ContactScreen!</Text>
+    </View>
+  );
+}
+
+export default ContactScreen;
